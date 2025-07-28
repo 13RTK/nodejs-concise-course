46a4f45a8d37444599bf9540271a6f941289a75f
alex <907785012@qq.com>
2025-07-28 20:00:53 +0800

    update event-loop section

--- deep-dive/evet-loop/event-loop-example.js
+++ deep-dive/evet-loop/event-loop-example.js
@@ -1,16 +1,18 @@
 import * as fsCallBack from 'node:fs';
 import * as fsPromise from 'node:fs/promises';
 
+// 1. Sync, execute in order
 const data = fsCallBack.readFileSync('./data.json', 'utf-8');
 
 console.log(data);
 console.log('Other things');
 
-fsCallBack.readFile('./data.json', 'utf-8', (error, data) => {
-  if (error) {
-    console.log(error.message);
-  }
-  console.log(data);
-});
+// 2. Async Callback
+// fsCallBack.readFile('./data.json', 'utf-8', (error, data) => {
+//   if (error) {
+//     console.log(error.message);
+//   }
+//   console.log(data);
+// });
 
-console.log('Other things');
+// console.log('Other things');
