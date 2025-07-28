fa5b79ebf2b3f0c128cfdf5db13743d1cb526a1b
alex <907785012@qq.com>
2025-07-28 17:43:53 +0800

    refactor event-loop section file structure

--- deep-dive/evet-loop/event-loop-example.js
+++ deep-dive/evet-loop/event-loop-example.js
@@ -5,3 +5,12 @@ const data = fsCallBack.readFileSync('./data.json', 'utf-8');
 
 console.log(data);
 console.log('Other things');
+
+fsCallBack.readFile('./data.json', 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error.message);
+  }
+  console.log(data);
+});
+
+console.log('Other things');
--- deep-dive/evet-loop/starter/event-loop-example.js
+++
@@ -1,7 +0,0 @@
-import * as fsCallBack from 'node:fs';
-import * as fsPromise from 'node:fs/promises';
-
-const data = fsCallBack.readFileSync('./data.json', 'utf-8');
-
-console.log(data);
-console.log('Other things');
